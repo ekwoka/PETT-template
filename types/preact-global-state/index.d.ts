9db67c8e726dcc86340e89ff987e594960212e12
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-05-29 16:53:54 +0400

    :heavy_plus_sign: :heavy_minus_sign: preact global state

--- types/preact-global-state/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'preact-global-state' {
-  const useGlobalState: <T>(key: string, defaultValue: T) => [T, (value: T) => void];
-  const store: Store;
-
-  export { useGlobalState, store };
-}
